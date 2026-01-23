1fd7ae27c21b02ae4d33ca69b2da1ce518b6440b
hansrachit123 <rhans@protocase.com>
2026-01-23 17:24:16 -0400

    fixed add connectio button format

--- cockpit-s3-browser/src/views/EndpointSetup.vue
+++ cockpit-s3-browser/src/views/EndpointSetup.vue
@@ -21,7 +21,7 @@
                 <TaskCenter></TaskCenter>
 
                 <button type="button"
-                  class="inline-flex items-center justify-center btn-primary rounded-md  px-4 py-2 text-sm font-semibold text-white shadow-sm hover:opacity-90 active:opacity-80 disabled:cursor-not-allowed disabled:opacity-60"
+                  class="inline-flex items-center justify-center btn-primary rounded-md px-4 py-2 text-sm font-semibold text-white shadow-sm hover:opacity-90 active:opacity-80 disabled:cursor-not-allowed disabled:opacity-60 flex-shrink-0"
                   :disabled="busy" @click="openCreate">
                   Add connection
                 </button>
